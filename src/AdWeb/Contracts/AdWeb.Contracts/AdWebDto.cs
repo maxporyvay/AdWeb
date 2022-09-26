f0fd0d9a1d18255eaf28b565c810b7349255cc09
sevma <sevma@DESKTOP-4RVQEKI>
2022-09-26 23:00:48 +0300

    Добавьте файлы проекта.

---
+++ src/AdWeb/Contracts/AdWeb.Contracts/AdWebDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdWeb.Contracts
+{
+    /// <summary>
+    /// Модель представления объявления.
+    /// </summary>
+    public class AdWebDto
+    {
+        /// <summary>
+        /// Идентификатор объявления.
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Заголовок объявления.
+        /// </summary>
+        public string AdTitle { get; set; }
+
+    }
+}
---
+++ src/AdWeb/Hosts/AdWeb.Api/Controllers/AdWebController.cs
@@ -0,0 +1,54 @@
+using AdWeb.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace AdWeb.Api.Controllers
+{
+    /// <summary>
+    /// Работа с доской объявлений.
+    /// </summary>
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class AdWebController : ControllerBase
+    {
+        public AdWebController()
+        {
+
+        }
+
+        /// <summary>
+        /// Возвращает объявления, размещенные пользователями.
+        /// </summary>
+        /// <returns>Коллекция элементов <see cref="AdWebDto"/>.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IReadOnlyCollection<AdWebDto>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAsync()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        /// <summary>
+        /// Удаляет объявление.
+        /// </summary>
+        /// <param name="id">Идентификатор объявления</param>
+        [HttpDelete("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        /// <summary>
+        /// Вносит изменения в объявление.
+        /// </summary>
+        /// <param name="id">Идентификатор объявления</param>
+        [HttpPut("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> UpdateAsync(Guid id)
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
